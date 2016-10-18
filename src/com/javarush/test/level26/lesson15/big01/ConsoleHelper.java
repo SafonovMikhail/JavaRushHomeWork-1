cc2a858dde80eb15882e94b60ccaf5ac9a96e5a3
sashamartian <sasha.martian@yandex.ru>
2016-10-18 11:19:32 +0700

    Done

---
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level26.lesson15.big01;
+
+import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String message = "";
+
+        try {
+            message = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return message;
+    }
+}
---
+++ src/com/javarush/test/level26/lesson15/big01/Operation.java
@@ -0,0 +1,5 @@
+package com.javarush.test.level26.lesson15.big01;
+
+public enum Operation {
+    INFO, DEPOSIT, WITHDRAW, EXIT
+}
