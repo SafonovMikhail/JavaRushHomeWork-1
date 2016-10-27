21e1d8e4f3ac92562f7b51efd44ed38ffed700ee
sashamartian <sasha.martian@yandex.ru>
2016-10-27 16:08:40 +0700

    Done

--- src/com/javarush/test/level26/lesson15/big01/CashMachine.java
+++ src/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -1,7 +1,23 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
+
+import java.util.Locale;
+
 public class CashMachine {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.ENGLISH);
+        try {
+            String s = ConsoleHelper.askCurrencyCode();
+            String[] w = ConsoleHelper.getValidTwoDigits(s);
+            int k = Integer.valueOf(w[0]);
+            int l = Integer.valueOf(w[1]);
+            CurrencyManipulator cm = new CurrencyManipulator(s);
+            cm.addAmount(k, l);
+
+        } catch (InterruptOperationException e) {
+            e.printStackTrace();
+        }
 
     }
 }
